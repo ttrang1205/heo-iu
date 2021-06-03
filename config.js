c5d7614bfb7fc0bee14b68de7dd8912aca74ef37
Nguyen Thi Thuy Trang <85290901+ttrang1205@users.noreply.github.com>
2021-06-03 19:31:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'Iuuu heooo lắm',
+    btnIntro: 'Iuuu bé lắm',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Heo nhớ bé lắm🥰🥰🥰',
